fe397af7a3ef3cec7ff2bc64e615386d2d61c6e0
Erik Johnson <airik.k.johnson@gmail.com>
2024-10-01 16:25:34 -0600

    added argument parsing to print_fires.py so things aren't hardcoded. Added docstrings and such too

--- print_fires.py
+++ print_fires.py
@@ -1,25 +1,95 @@
+""" 
+Module that contains 
+(1) command line parser
+(2) wrapper function that, given the Agrofood_co2_emission.csv, a country, and an 
+    emissions column, prints the total emissions in the country.
+"""
+
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
 from utils import get_rows_by_column_value
 
-country = "United States of America"
-country_column_index = 0
-file_path = Path("./Agrofood_co2_emission.csv")
-savanna_emissions_colm = 2
 
-usa_rows = get_rows_by_column_value(
-    file_path=file_path, column_value=country, column_index=country_column_index
-)
+def parse_command_line_args() -> Namespace:
+    """
+    Parse command line arguments
 
-savanna_emissions = [float(row[savanna_emissions_colm]) for row in usa_rows]
-# To SWE4S folks:
-#   The above line uses "list comprehension" which is
-#   is a concise way to create a new list by applying an expression to each element of an
-#   existing iterable, optionally including a conditional filter
-#   the expression.
-#   You could alternatively have used a more conventional for loop but the above is more concise
-#   and, once you get used to the syntax, just as clear
+    Returns:
+        Namespace: the parsed command line arguments
+    """
+    parser = ArgumentParser(
+        description=(
+            "Given the Agrofood_co2_emission.csv dataset, this script prints the sum of a "
+            "given column for a given country. "
+            "It can be used, for example, to determine the total emissions from savanna fires "
+            "in the United States of America."
+        )
+    )
+    parser.add_argument(
+        "--country_column",
+        type=int,
+        help="The index of the country column in the CSV",
+        required=True,
+    )
+    parser.add_argument(
+        "--country",
+        type=str,
+        help="The country whose data you are interested in",
+        required=True,
+    )
+    parser.add_argument(
+        "--emissions_column",
+        type=int,
+        help="The index of the emissions column in the CSV",
+        required=True,
+    )
+    parser.add_argument(
+        "--file_path",
+        type=str,
+        help="The path to the Agrofood_co2_emission.csv",
+        required=True,
+    )
 
-print(
-    f"In the {file_path.stem} dataset, the total emissions from savanna fires in {country} is {sum(savanna_emissions)}."
-)
+    args = parser.parse_args()
+
+    return args
+
+
+def main(csv_path: Path, country: str, country_colm: int, emissions_colm: int):
+    """
+    Workhorse function that:
+        (1) gets the rows of a CSV that correspond to the given country
+        (2) computes the total emissions from a given column for the given country
+
+    Args:
+        csv_path (Path): path to CSV
+        country (str): name of the country
+        country_colm (int): index of country column in the CSV
+        emissions_colm (int): index of the emissions column in the CSV
+    """
+    rows = get_rows_by_column_value(
+        file_path=csv_path, column_value=country, column_index=country_colm
+    )
+
+    total_emissions = 0
+    for row in rows:
+        try:
+            total_emissions += float(row[emissions_colm])
+        except ValueError:
+            raise ValueError(
+                f"Can not convert {row[emissions_colm]} in column {emissions_colm} to a float"
+            )
+    print(
+        f"In the {csv_path.name} dataset, for country='{country}', the sum of values in column {emissions_colm} is {total_emissions}."
+    )
+
+
+if __name__ == "__main__":
+    args = parse_command_line_args()
+    main(
+        csv_path=Path(args.file_path),
+        country=args.country,
+        country_colm=args.country_column,
+        emissions_colm=args.emissions_column,
+    )
--- utils.py
+++ utils.py
@@ -1,10 +1,38 @@
-def get_rows_by_column_value(file_path, column_value, column_index=0):
-    # In CSV, get all the rows where row[column_index] == column_value
-    with open(file_path, "r") as file:
-        matching_rows = []
-        for line in file:
-            row = line.split(",")
-            if row[column_index] == column_value:
-                matching_rows.append(row)
+""" 
+Module containing utility/helper functions to facilitate extracting information from 
+Agrofood_co2_emission.csv
+"""
 
-        return matching_rows
+from pathlib import Path
+from typing import List
+
+
+def get_rows_by_column_value(
+    file_path: Path, column_value: str, column_index: int = 0
+) -> List[List[str]]:
+    """
+    Read assumed CSV file line-by-line. Split each line by "," into a list of strings.
+    Calling each line's list of strings 'row', return those rows for which
+    row[colum_index] == column_value.
+
+    Args:
+        file_path (Path): path to CSV
+        column_value (str): query value
+        column_index (int, optional): index of column to query on. Defaults to 0.
+
+    Returns:
+        List[List[str]]: rows for which row[colum_index] == column_value.
+    """
+    try:
+        with open(file_path, "r") as file:
+            matching_rows = []
+            for line in file:
+                row = line.split(",")
+                if row[column_index] == column_value:
+                    matching_rows.append(row)
+
+            return matching_rows
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            f"Could not find file {file_path}. Please provide a valid path"
+        )
