ce371a6b5fa80560f41eb654cc694f22c1ca544c
Erik Johnson <airik.k.johnson@gmail.com>
2024-10-01 14:52:04 -0600

    updated print_fires.py to print the total emissions from savanna fires in the USA; updated the README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# python-refresher
+# HW 2: Python refresher
+
+## Usage
+
+The Python script `print_fires.py` prints the total emissions from savanna fires in the USA in the [`Agri-food CO2 emission dataset`](https://www.kaggle.com/datasets/alessandrolobello/agri-food-co2-emission-dataset-forecasting-ml).
+
+To run the script, make sure `print_fires.py` and `Agrofood_co2_emission.csv` are in your current directory and then run `python print_fires.py`.
+
+## Changelog
+
+### v1.0.0
+
+- Added `print_fires.py`
--- print_fires.py
+++ print_fires.py
@@ -1,7 +1,25 @@
+from pathlib import Path
+
+from utils import get_rows_by_column_value
+
 country = "United States of America"
-country_column_index = -1  # TODO: update this
-file_path = "./Agrofood_co2_emission.csv"
-# TODO
-# add code to compute the number of fires so that the following print statement prints
-# the number of fires in the given country
-print(f"There were {num_fires} fires in {country}")
+country_column_index = 0
+file_path = Path("./Agrofood_co2_emission.csv")
+savanna_emissions_colm = 2
+
+usa_rows = get_rows_by_column_value(
+    file_path=file_path, column_value=country, column_index=country_column_index
+)
+
+savanna_emissions = [float(row[savanna_emissions_colm]) for row in usa_rows]
+# To SWE4S folks:
+#   The above line uses "list comprehension" which is
+#   is a concise way to create a new list by applying an expression to each element of an
+#   existing iterable, optionally including a conditional filter
+#   the expression.
+#   You could alternatively have used a more conventional for loop but the above is more concise
+#   and, once you get used to the syntax, just as clear
+
+print(
+    f"In the {file_path.stem} dataset, the total emissions from savanna fires in {country} is {sum(savanna_emissions)}."
+)
