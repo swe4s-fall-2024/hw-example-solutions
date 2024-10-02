d6d4a8ea82a8e36ce5dc3b391ce899af1d02590f
PSanthosh22 <pasa5327@colorado.edu>
2024-10-02 16:46:00 -0600

    working on draft

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ Agrofood_co2_emission.csv
 
 from pathlib import Path
 from typing import List
+import statistics
 
 
 def get_rows_by_column_value(
@@ -36,3 +37,31 @@ def get_rows_by_column_value(
         raise FileNotFoundError(
             f"Could not find file {file_path}. Please provide a valid path"
         )
+
+def get_data_points(rows,emissions_colm):
+    data_points = []
+    for row in rows:
+        try:
+            data_points.append(float(row[emissions_colm]))
+        except ValueError:
+            raise ValueError(
+                f"Can not convert {row[emissions_colm]} in column {emissions_colm} to a float"
+            )
+    return data_points
+   
+def find_sum(data_points):
+    total_emissions = 0
+    for point in data_points:
+        total_emissions += point
+    return total_emissions
+
+def find_mean(data_points):
+    mean_emissions = statistics.mean(data_points)
+    return mean_emissions
+
+def find_standard_dev(data_points):
+
+    return
+
+def find_median(data_points):
+    return
